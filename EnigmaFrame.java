cf37970cf39b6b0ae96ea7e1571be1f8936d4e43
Andre <andreferraz@ferrazbrasil.com.br>
2024-11-18 14:48:39 -0500

    added EnigmaGUI.java and EnigmaFrame.java

---
+++ EnigmaFrame.java
@@ -0,0 +1,26 @@
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class EnigmaFrame {
+
+    private JComboBox innerChooser;
+    private JComboBox middleChooser;
+    private JComboBox outerChooser;
+
+    private JTextField startingChars;
+
+    private JButton encrypt;
+    private JButton decrypt;
+
+    private JPanel layout;
+
+    private JLabel innerSelectLabel;
+    private JLabel outerSelectLabel;
+    private JLabel middleSelectLabel;
+    private JLabel inputText;
+    private JLabel outputText;
+
+}
