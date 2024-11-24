ececf255ed3a3cfb06c16e8502692e25d4714877
Andre <andreferraz@ferrazbrasil.com.br>
2024-11-23 22:59:54 -0500

    Changed the layout type and added all the components needed

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -8,12 +8,15 @@ import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
 public class EnigmaFrame extends JFrame {
 
-    private JComboBox innerChooser;
-    private JComboBox middleChooser;
-    private JComboBox outerChooser;
+    String[] comboPatterns = { "1", "2", "3", "4", "5" };
+    private JComboBox innerChooser = new JComboBox<String>(comboPatterns);
+    private JComboBox middleChooser = new JComboBox<String>(comboPatterns);
+    private JComboBox outerChooser = new JComboBox<String>(comboPatterns);
 
     private JTextField startingChars = new JTextField();
     private JTextArea outputTextArea = new JTextArea();
@@ -29,25 +32,39 @@ public class EnigmaFrame extends JFrame {
     private JLabel innerSelectLabel = new JLabel("Inner rotor");
     private JLabel outerSelectLabel = new JLabel("Outer rotor");
     private JLabel middleSelectLabel = new JLabel("Middle rotor");
+    private JLabel startingCharlabel = new JLabel("Starting Chars");
     private JLabel inputText = new JLabel("Input");
     private JLabel outputText = new JLabel("Output");
 
     public EnigmaFrame() {
         super();
-        this.setLayout(new BorderLayout());
-        inputSection.setLayout(new BorderLayout());
-        outputSection.setLayout(new BorderLayout());
-        selectionSection.setLayout(new BorderLayout());
+        this.setLayout(new GridLayout(3, 1, 25, 25));
+        inputSection.setLayout(new GridLayout(1, 2));
+        outputSection.setLayout(new GridLayout(1, 2));
+        selectionSection.setLayout(new GridLayout(1, 10, 10, 5));
 
-        inputSection.add(inputText, BorderLayout.WEST);
-        inputSection.add(intputTextField, BorderLayout.EAST);
+        selectionSection.add(innerSelectLabel);
+        selectionSection.add(innerChooser);
+        selectionSection.add(middleSelectLabel);
+        selectionSection.add(middleChooser);
+        selectionSection.add(outerSelectLabel);
+        selectionSection.add(outerChooser);
+        selectionSection.add(startingCharlabel);
+        selectionSection.add(startingChars);
+        selectionSection.add(encrypt);
+        selectionSection.add(decrypt);
 
-        outputSection.add(outputText, BorderLayout.WEST);
-        outputSection.add(outputTextArea, BorderLayout.EAST);
+        inputSection.add(inputText);
+        inputSection.add(intputTextField);
+        intputTextField.setPreferredSize(new Dimension(200, 30));
 
-        this.add(selectionSection, BorderLayout.NORTH);
-        this.add(inputSection, BorderLayout.CENTER);
-        this.add(outputSection, BorderLayout.SOUTH);
+        outputSection.add(outputText);
+        outputSection.add(outputTextArea);
+        outputTextArea.setPreferredSize(new Dimension(200, 30));
+
+        this.add(selectionSection);
+        this.add(inputSection);
+        this.add(outputSection);
 
         this.pack();
     }
--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,7 +1,12 @@
+import javax.swing.JFrame;
+
 public class EnigmaGUI {
 
     public static void main(String[] args) {
         EnigmaFrame frame = new EnigmaFrame();
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
